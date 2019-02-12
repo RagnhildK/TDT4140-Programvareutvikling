66148bf41b32c3032c05738975efdbeb3acaedfc
Ingebrigt Nygård <ingebrin@stud.ntnu.no>
2019-02-12 17:11:34 +0100

    Prøver å få til innlogging fra database siden

--- src/Database.java
+++ src/Database.java
@@ -8,6 +8,66 @@ public class Database {
     static final String USER = "root";
     static final String PASS = "pu16";
 
+    public boolean checkLogin(String username, String password) {
+        Connection conn = null;
+        Statement stmt = null;
+        String passord = "password";
+
+        try {
+            //STEP 3: Open a connection
+            Class.forName(JDBC_DRIVER);
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT Passord FROM Bruker WHERE BrukerNavn = 'admin'";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            passord = rs.getString("Passord");
+
+            /*
+            while(rs.next()){
+                //Retrieve by column name
+                String navn = rs.getString("Navn");
+
+                //Display values
+                System.out.println("Navn: " + navn);
+            }
+            */
+
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+        if (passord.equals(password)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    /*
     public static void main(String[] args) {
         Connection conn = null;
         Statement stmt = null;
@@ -22,7 +82,7 @@ public class Database {
             System.out.println("Creating statement...");
             stmt = conn.createStatement();
             String sql;
-            sql = "SELECT Navn FROM Bruker";
+            sql = "SELECT Navn FROM Bruker WHERE Rolle = 'admin'";
             ResultSet rs = stmt.executeQuery(sql);
 
             //STEP 5: Extract data from result set
@@ -56,6 +116,6 @@ public class Database {
         }//end try
         System.out.println("Goodbye!");
 
-    }
+    }*/
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Main {
 
     //This is the main class
@@ -17,13 +22,38 @@ public class Main {
         return x * y;
     }
 
+    public String Input(String what) throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter " + what + ":");
+            String s = br.readLine();
+            return s;
+            /*
+            System.out.print("Enter Integer:");
+            try{
+                int i = Integer.parseInt(br.readLine());
+            }catch(NumberFormatException nfe){
+                System.err.println("Invalid Format!");
+            }*/
+    }
 
-    public static void main(String[] args) {
-        Main hW = new Main();
-        System.out.println(hW.helloWorld());
-        System.out.println(hW.add(4,5));
-        Person p = new Person("Bob", 12);
-        System.out.println(p.getName());
+
+    public static void main(String[] args) throws IOException{
+        Main m = new Main();
+        Database db = new Database();
+        while (true){
+            String username = m.Input("username");
+            if (username.equals("quit")){
+                break;
+            }
+            String password = m.Input("password");
+            System.out.print(username+":"+password);
+            if(db.checkLogin(username,password)){
+                System.out.print("Login success!");
+            }
+            else {
+                System.out.print("Login failed!");
+            }
+        }
 
 
     }
