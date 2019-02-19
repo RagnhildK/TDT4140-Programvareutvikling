ab08bfaf032cd22d1f2bcd10d8182f5d749df621
bjorn <bjornhsp@stud.ntnu.no>
2019-02-19 12:36:01 +0100

    FaglærerController

---
+++ src/main/java/FaglærerController.java
@@ -0,0 +1,18 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+
+public class FaglærerController {
+
+    @FXML public DatePicker date;
+    @FXML public TextField txtFra;
+    @FXML public TextField txtTil;
+    @FXML public TextField txtEmneID;
+    @FXML public TextField txtTidPerStudent;
+    @FXML public Button btnLeggTil;
+
+
+
+
+
+
+}
